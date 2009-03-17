1e7fe42ba773f786e1c9fe057e8f2ee1706b3a4e
宣言 <xuanyan1983@gmail.com>
2009-03-17 16:49:32 +0800

    . typo

--- test.php
+++ test.php
@@ -2,27 +2,26 @@
 
 /* www.kukufun.com Session Class-test by xuanyan <xunayan1983@gmail.com> */
 
-// require './Session.Pdo.php';
+require './Session.php';
 // 
-// $db_file = dirname(__FILE__) . '/session.db';
+$db_file = dirname(__FILE__) . '/session.db';
 // 
-// $pdo = new PDO('sqlite://' . $db_file);
+$pdo = new PDO('sqlite://' . $db_file);
+
+Session::start($pdo);
 
 
-// Session::start($pdo);
-
-
-require './Session.Memcache.php';
-
-$mc = new Memcache();
-$mc->connect('192.168.13.32', 11211);
-Session::start($mc);
-
-
+// require './Session.Memcache.php';
+// 
+// $mc = new Memcache();
+// $mc->connect('192.168.13.32', 11211);
+// Session::start($mc);
+// 
+// 
 print_r($_SESSION);
-
+// 
 $_SESSION['now'] = time();
-
+// 
 $_SESSION['count'] = empty($_SESSION['count']) ? 1 : $_SESSION['count'] + 1;
 
 //session_destroy();
